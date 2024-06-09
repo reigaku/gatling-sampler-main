0afbe743292d584b73ac16fd8cf68d6fc3632149
reigaku <74679900+reigaku@users.noreply.github.com>
2024-06-09 21:57:10 +0300

    negr

--- src/test/scala/vc/Actions.scala
+++ src/test/scala/vc/Actions.scala
@@ -5,8 +5,12 @@ import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder
 
 object Actions {
-
   val getMainPage: HttpRequestBuilder = http("getMainPage")
+    .get("/cgi-bin/welcome.pl")
+    .queryParam("signOff", "true")
+    .check(status is 200)
+
+  val getMainPage1: HttpRequestBuilder = http("getMainPage1")
     .get("/cgi-bin/nav.pl")
     .queryParam("query", "in=home")
     .check(status is 200)
@@ -15,36 +19,55 @@ object Actions {
     )
 
   val login: HttpRequestBuilder = http("login")
-    .post("*/cgi-bin/login.pl")
+    .post("/cgi-bin/login.pl")
     .formParam("userSession", "#{userSession}")
     .formParam("username", "chudo")
     .formParam("password", "negr")
 
+  val login1: HttpRequestBuilder = http("login1")
+    .post("/cgi-bin/nav.pl")
+    .formParam("page", "menu")
+    .formParam("in", "home")
 
 
-  val subs: HttpRequestBuilder = http("getSubs")
-    .get("/subs")
-    .queryParam("mode", "ajax")
-    .check(regex("""Ещё (\d+)&nbsp;подпис\W+<""").is("884"))
-    .check(regex("""Ещё (\d+)&nbsp;подпис\W+<""").saveAs("counter"))
+  val login2: HttpRequestBuilder = http("login2")
+    .post("/cgi-bin/login.pl")
+    .formParam("intro", "true")
 
-  val search: HttpRequestBuilder = http("search #{counter}")
-    .get("/search/v2/content/relevant")
-    .queryParam("mode", "ajax")
-    .queryParam("query", "#{word}")
-    .check(status not 500)
-    .check(regex("""l-page l-page--header-content-sidebar l-mb-12""").exists)
+  val flights_page: HttpRequestBuilder = http("flights_page")
+    .post("/cgi-bin/welcome.pl")
+    .formParam("page", "search")
+  val flights_page1: HttpRequestBuilder = http("flights_page1")
+    .post("/cgi-bin/nav.pl")
+    .formParam("page", "menu")
+    .formParam("in", "flights")
+  val flights_page2: HttpRequestBuilder = http("flights_page2")
+    .post("/cgi-bin/reservations.pl")
+    .formParam("page", "welcome")
+    .check(
+      regex("(?<=<option value=\")[^\"]+").findRandom.saveAs("cityOut")
+    )
+    .check(
+      regex("(?<=<option value=\")[^\"]+").findRandom.saveAs("cityTo")
+    )
+
+  val reserve_flight: HttpRequestBuilder = http("reserve_flight")
+    .post("/cgi-bin/reservations.pl")
+    .formParam("advanceDiscount", "0")
+    .formParam("depart", "#{cityOut}")
+    .formParam("departDate", "05/01/2024")
+    .formParam("arrive", "#{cityTo}")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
+    .formParam("page", "welcome")
 
 
 
-  val check: HttpRequestBuilder = http("check")
-    .get("/auth/check")
-    .queryParam("mode", "raw")
-    .check(jsonPath("$.rc").is("200"))
-
-  val notifications: HttpRequestBuilder = http("notifications")
-    .get("/notifications/unread-count")
-    .queryParam("mode", "raw")
-    .check(jsonPath("$.rc").is("200"))
 
 }
--- src/test/scala/vc/CommonScenario.scala
+++ src/test/scala/vc/CommonScenario.scala
@@ -11,8 +11,15 @@ object CommonScenario{
 
 class CommonScenario {
   val scn: ScenarioBuilder = scenario("Common scenario")
-  exec(Actions.getMainPage)
-  exec(Actions.login)
+    .exec(Actions.getMainPage)
+  .exec(Actions.getMainPage1)
+  .exec(Actions.login)
+    .exec(Actions.login)
+    .exec(Actions.login1)
+    .exec(Actions.login2)
+    .exec(Actions.flights_page)
+    .exec(Actions.flights_page1)
+    .exec(Actions.flights_page2)
 
 
   //val loginGroup: ChainBuilder = group("my login"){
